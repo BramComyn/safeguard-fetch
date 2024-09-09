60498fc05bdee35ae0a4543132b227461fa83ece
Bram Comyn <bram.comyn@ugent.be>
2024-09-09 10:25:05 +0200

    refactor: organising in scripts-directory

--- scripts/playgrounds/attackServerPlayground.ts
+++ scripts/playgrounds/attackServerPlayground.ts
@@ -1,22 +1,22 @@
 import type { Server } from 'node:http';
 import type { Http2SecureServer } from 'node:http2';
 
-import { secureServerOptions } from '../src/util';
-import { AttackServerHttpFactory } from '../src/attack-server/attack-server-factory/AttackServerHttpFactory';
+import { secureServerOptions } from '../../src/util';
+import { AttackServerHttpFactory } from '../../src/attack-server/attack-server-factory/AttackServerHttpFactory';
 
 import {
   AttackServerHttp2SecureFactory,
-} from '../src/attack-server/attack-server-factory/AttackServerHttp2SecureFactory';
+} from '../../src/attack-server/attack-server-factory/AttackServerHttp2SecureFactory';
 
 import {
   AttackServerHttpInitialiser,
-} from '../src/attack-server/attack-server-initialiser/AttackServerHttpInitialiser';
+} from '../../src/attack-server/attack-server-initialiser/AttackServerHttpInitialiser';
 
 import {
   AttackServerHttp2Initialiser,
-} from '../src/attack-server/attack-server-initialiser/AttackServerHttp2Initialiser';
+} from '../../src/attack-server/attack-server-initialiser/AttackServerHttp2Initialiser';
 
-import { AttackServer } from '../src/attack-server/attack-server/AttackServer';
+import { AttackServer } from '../../src/attack-server/attack-server/AttackServer';
 
 const httpserver = new AttackServer<Server>(8080, new AttackServerHttpFactory(), new AttackServerHttpInitialiser());
 httpserver.startServer();
