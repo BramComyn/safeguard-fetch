31e278b392428090cbed8a0c1486f5cbfbacbb02
Bram Comyn <bram.comyn@ugent.be>
2024-09-13 10:41:00 +0200

    test: implemented tests for RefuseNoContentLengthHandler

--- test/unit/handler/RefuseNoContentLengthHandler.test.ts
+++ test/unit/handler/RefuseNoContentLengthHandler.test.ts
@@ -1,9 +1,39 @@
+import type { ClientHttp2Stream, IncomingHttpHeaders } from 'node:http2';
+
+import {
+  RefuseNoContentLengthHandler,
+} from '../../../src/handler/response/RefuseNoContentLengthHandler';
+
 describe('RefuseNoContentLengthHandler', (): void => {
-  it.todo(
+  let handler: RefuseNoContentLengthHandler;
+  let request: jest.Mocked<ClientHttp2Stream>;
+  let headers: jest.Mocked<IncomingHttpHeaders>;
+
+  beforeEach((): void => {
+    handler = new RefuseNoContentLengthHandler();
+    request = {
+      close: jest.fn(),
+    } as any;
+  });
+
+  it(
     'should close the request if the content length is not provided.',
+    (): void => {
+      headers = {};
+      handler.handle(request, headers);
+      expect(request.close).toHaveBeenCalledTimes(1);
+    },
   );
 
-  it.todo(
+  it(
     'should not close the request if the content length is provided.',
+    (): void => {
+      headers = {
+        'content-length': '200',
+      };
+
+      handler.handle(request, headers);
+      expect(request.close).not.toHaveBeenCalled();
+    },
   );
 });
