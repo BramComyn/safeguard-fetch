de25e57208a32267a3bb5893c4b633bda5c1665a
Bram Comyn <bram.comyn@ugent.be>
2024-09-17 09:01:45 +0200

    docs: added clearifying comment

--- src/response-generator/JsonLdResponseGenerator.ts
+++ src/response-generator/JsonLdResponseGenerator.ts
@@ -5,6 +5,9 @@ import type { Readable } from 'node:stream';
 import { fromRoot } from '../util';
 import type { ResponseGenerator } from './ResponseGenerator';
 
+/**
+ * A response generator that returns a JSON-LD response.
+ */
 export class JsonLdResponseGenerator implements ResponseGenerator {
   public generateResponse(): { headers: OutgoingHttpHeaders; body: Readable } {
     const data = createReadStream(fromRoot('assets/data.jsonld'));
